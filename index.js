290c17312f75fe3f78db7abe69a1054e146bca63
Nicole Gomez <nicolegomezx3@gmail.com>
2023-04-09 23:00:06 -0400

    passed three tests

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+document.body.append(newHeader);
+const text = document.getElementById("victory");
+text.innerHTML += "Nicole is the champion"
+
+
+
+//const element = document.createElement("div");
+//document.body.append(element);
+
+//const ul = document.createElement("ul");
+//for (let i = 0; i < 3; i++) {
+   // const li = document.createElement("li");
+    //li.textContent = (i + 1).toString();
+    //li.textContent = (i + 1).toString();
+    //ul.append(li);
+    //li.textContent = "Hi there!";
+//    element.append(ul);
+//}
+
+//const main = document.getElementById("main");
+//main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+//const elements = document.getElementById('main');
+//element.style.height = "300px";
+//element.style.backgroundColor = "#27647B";
+//element.textContent = "You've changed what's on the screen!";
+
+//element.style.fonSize = "24px";
+//element.style.marginLeft = "30px";
+//element.style.lineHeight = 2;
+
+//element.className = "pet-listing dog";
+//element.classList.remove("dog");
+//element.classList.add("cat", "sale");
